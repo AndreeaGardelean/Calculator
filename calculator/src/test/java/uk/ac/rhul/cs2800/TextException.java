a57c6e50f4054c63c9593560ea6c13d373152dcf
Andreea Gardelean <andreea.gardelean.2020@live.rhul.ac.uk>
2022-10-28 10:05:02 +0100

    First part of calculator project for CS2800

---
+++ calculator/src/test/java/uk/ac/rhul/cs2800/TextException.java
@@ -0,0 +1,15 @@
+package uk.ac.rhul.cs2800;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import org.junit.jupiter.api.Test;
+
+class TextException {
+
+  // Test 1
+  // Test if the exception can be thrown
+  @Test
+  void test() throws BadTypeException {
+    Entry en = new Entry(4.34f, Type.NUMBER);
+    assertThrows(BadTypeException.class, () -> en.getStr(), "The entry is not of type String, cannot call String getter on a float type");
+  }
+}
